8a0953760e1f234c69d37549508711065b0c5432
Alexandr Buzin <buzin783944@mail.ru>
2019-02-27 17:46:41 +0400

    checked POST AND GET METHODS

--- src/main/java/com/atlassian/plugins/report/rest/ReportCreateResource.java
+++ src/main/java/com/atlassian/plugins/report/rest/ReportCreateResource.java
@@ -1,6 +1,9 @@
 package com.atlassian.plugins.report.rest;
 
+import com.atlassian.jira.util.json.JSONException;
+import com.atlassian.jira.util.json.JSONObject;
 import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
+import com.google.gson.JsonObject;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -12,11 +15,13 @@ import javax.ws.rs.core.Response;
 @Path("/message")
 public class ReportCreateResource {
 
-    @GET
+    @POST
     @AnonymousAllowed
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public Response getMessage()
+    @Consumes({ MediaType.APPLICATION_JSON,MediaType.APPLICATION_FORM_URLENCODED})
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("/name")
+    public Response getMessage(String data)
     {
-       return Response.ok(new ReportCreateResourceModel("Hello World")).build();
+        return Response.ok(data).build();
     }
 }
